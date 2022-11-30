3ae2a7f0f3cb55ba7c8f860c07bbc17dff2ea1ac
Ibraguim Albakov <ibraguimalbakov@MacBook-Air-de-Ibraguim.local>
2022-11-30 23:01:37 +0100

    home_1

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "minishell.h"
+
+
+int main(int ac, char **av, char **env)
+{
+
+
+
+}
---
+++ minishell.h
@@ -0,0 +1,21 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
